0b75481ca803f21e0f482691c04e210781f63f29
Sameerjain_084 <Jainsameer179@gmail.com>
2026-09-02 23:49:42 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.81%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,20 +11,42 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans){
-        if(root == NULL){
-            return;
-        }
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root,ans);
+        TreeNode* curr = root;
+
+        while (curr != NULL) {
+
+            // Case 1: No left subtree
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+
+            // Case 2: Left subtree exists
+            else {
+                // Find inorder predecessor
+                TreeNode* IP = curr->left;
+
+                while (IP->right != NULL && IP->right != curr) {
+                    IP = IP->right;
+                }
+
+                // Create thread
+                if (IP->right == NULL) {
+                    IP->right = curr;
+                    curr = curr->left;
+                }
+
+                // Remove thread
+                else {
+                    IP->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
 
         return ans;
-
-        
     }
 };
